9e91113d07ba87d91d1b1ae2f346d66082ab8b54
Nils Heilemann <nils.heilemann@nimbits.de>
2022-09-25 22:15:47 +0200

    code beautified

--- streaming_data_plattform/server_pConsum.py
+++ streaming_data_plattform/server_pConsum.py
@@ -7,8 +7,6 @@ sys.path.insert(0, "..")
 from asyncua import ua, Server
 from asyncua.common.methods import uamethod
 
-
-
 @uamethod
 def func(parent, value):
     return value * 2
@@ -34,19 +32,11 @@ async def main():
     # server.nodes, contains links to very common nodes like objects and root
     obj = await server.nodes.objects.add_object(idx, 'SEHO Sensors')
     varDateTime = await obj.add_variable(idx, 'dateTime', 0, ua.VariantType.String)
-    #varOutput = await obj.add_variable(idx, 'output (kWh)', 0, ua.VariantType.Double)
-    #varBConsum = await obj.add_variable(idx, 'basicConsumption (kWh)', 0, ua.VariantType.Double)
-    #varMConsum = await obj.add_variable(idx, 'managementConsumption (kWh)', 0, ua.VariantType.Double)
     varPConsum = await obj.add_variable(idx, 'productionConsumption (kWh)', 0, ua.VariantType.Double)
-    
-    
+
     # Set Variables to be writable by clients
     await varDateTime.set_writable()
-    #await varOutput.set_writable()
-    #await varBConsum.set_writable()
-    #await varMConsum.set_writable()
     await varPConsum.set_writable()
-    
     
     await server.nodes.objects.add_method(ua.NodeId('ServerMethod', 2), ua.QualifiedName('ServerMethod', 2), func, [ua.VariantType.Int64], [ua.VariantType.Int64])
     _logger.info('Starting server!')
@@ -54,24 +44,15 @@ async def main():
         while True:
             for row in sensor_data.itertuples():
                 valDateTime = row[1]
-                #valOutput = row[2]
-                #valBConsum = row[3]
-                #valMConsum = row[4]
                 valPConsum = row[2]
-                
-                print(valDateTime)
-                
-                #_logger.info('Set value of %s to %.1f', myvar, new_val)
+
+                # giving values
                 await varDateTime.write_value(valDateTime)
-                #await varOutput.write_value(valOutput)
-                #await varBConsum.write_value(valBConsum)
-                #await varMConsum.write_value(valMConsum)
                 await varPConsum.write_value(valPConsum)
-                
+
+                # wait 1 second before repeating
                 await asyncio.sleep(1)
                 
 if __name__ == '__main__':
-
     logging.basicConfig(level=logging.DEBUG)
-
     asyncio.run(main(), debug=True)
