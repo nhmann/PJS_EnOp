675077a49bea4061efb35794ad03cbecb05327c1
Nils Heilemann <nils.heilemann@nimbits.de>
2022-09-25 21:38:07 +0200

    Initial release

---
+++ streaming_data_plattform/client_producer_bConsum.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  9 15:28:35 2022
+
+@author: nilsheilemann
+"""
+import asyncio
+import logging
+import pandas as pd
+from asyncua import Client, Node, ua
+import sys
+sys.path.insert(0, "..")
+
+import matplotlib.cbook
+import warnings
+
+warnings.filterwarnings("ignore",category=matplotlib.cbook.mplDeprecation)
+
+from datetime import datetime
+from kafka import KafkaProducer
+
+logging.basicConfig(level=logging.INFO)
+_logger = logging.getLogger('asyncua')
+
+
+class SubHandler(object):
+    # Subscription Handler.
+    def datachange_notification(self, node, val, data):
+        print("New data change event", node, val)
+
+    def event_notification(self, event):
+        print("New event", event)
+
+
+async def main():
+    # Connect with opc-ua server
+    url = 'opc.tcp://0.0.0.0:4840/opcua/server_bConsum/'
+    client = Client(url=url)
+
+    # Create a Kafka Producer
+    producer = KafkaProducer(bootstrap_servers='localhost:9092')
+    
+    
+    async with client:
+
+        uri = 'http://pjs.uni-wue.de'
+        idx = await client.get_namespace_index(uri)
+ 
+        varDateTime = await client.nodes.root.get_child(["0:Objects", f"{idx}:SEHO Sensors", f"{idx}:dateTime"])
+        #varOutput = await client.nodes.root.get_child(["0:Objects", f"{idx}:SEHO Sensors", f"{idx}:output (kWh)"])
+        varBConsum = await client.nodes.root.get_child(["0:Objects", f"{idx}:SEHO Sensors", f"{idx}:basicConsumption (kWh)"])
+        #varMConsum = await client.nodes.root.get_child(["0:Objects", f"{idx}:SEHO Sensors", f"{idx}:managementConsumption (kWh)"])
+        #varPConsum = await client.nodes.root.get_child(["0:Objects", f"{idx}:SEHO Sensors", f"{idx}:productionConsumption (kWh)"])
+
+        
+        # subscribing to a variable node
+        handler = SubHandler()
+        sub = await client.create_subscription(10, handler)
+        handle0 = await sub.subscribe_data_change(varDateTime)
+        #handle1 = await sub.subscribe_data_change(varOutput)
+        handle2 = await sub.subscribe_data_change(varBConsum)
+        #handle3 = await sub.subscribe_data_change(varMConsum)
+        #handle4 = await sub.subscribe_data_change(varPConsum)
+        await asyncio.sleep(0.1)
+        
+        # subscribe to events from server
+        await sub.subscribe_events()
+
+        
+        while True:
+            await asyncio.sleep(1)
+            
+            #convert DateTime String to type DateTime object
+            dateTimeStr = await varDateTime.read_value()
+            
+            # Assign Values
+            dateTimeValue = datetime.strptime(dateTimeStr, '%Y-%m-%d %H:%M:%S')
+            #outputValue = await varOutput.read_value()
+            BConsumValue = await varBConsum.read_value()
+            #MConsumValue = await varMConsum.read_value()
+            #PConsumValue = await varPConsum.read_value()
+
+            
+            """ Push to Kafka """
+            # Push DateTime as Key and basic consumption (kWh) as Value
+            producer.send('bConsum', key=bytes(str(dateTimeValue), 'utf-8'), value=bytes(str(BConsumValue), 'utf-8'))
+
+if __name__ == '__main__':
+    asyncio.run(main())
---
+++ streaming_data_plattform/server_bConsum.py
@@ -0,0 +1,77 @@
+import logging
+import asyncio
+import sys
+import pandas as pd
+sys.path.insert(0, "..")
+
+from asyncua import ua, Server
+from asyncua.common.methods import uamethod
+
+
+
+@uamethod
+def func(parent, value):
+    return value * 2
+
+
+async def main():
+    _logger = logging.getLogger('asyncua')
+    # setup our server
+    server = Server()
+    await server.init()
+    server.set_endpoint('opc.tcp://0.0.0.0:4840/opcua/server_bConsum/')
+
+    # setup our own namespace, not really necessary but should as spec
+    uri = 'http://pjs.uni-wue.de'
+    idx = await server.register_namespace(uri)
+
+    # Read Sensor Data
+    df = pd.read_csv("sensor.csv")
+    # Only use sensor data from 03 and 01 (preference)
+    sensor_data = pd.concat([df["dateTime"], df["basicConsumption (kWh)"]], axis=1)
+    
+    # populating our address space
+    # server.nodes, contains links to very common nodes like objects and root
+    obj = await server.nodes.objects.add_object(idx, 'SEHO Sensors')
+    varDateTime = await obj.add_variable(idx, 'dateTime', 0, ua.VariantType.String)
+    #varOutput = await obj.add_variable(idx, 'output (kWh)', 0, ua.VariantType.Double)
+    varBConsum = await obj.add_variable(idx, 'basicConsumption (kWh)', 0, ua.VariantType.Double)
+    #varMConsum = await obj.add_variable(idx, 'managementConsumption (kWh)', 0, ua.VariantType.Double)
+    #varPConsum = await obj.add_variable(idx, 'productionConsumption (kWh)', 0, ua.VariantType.Double)
+    
+    
+    # Set Variables to be writable by clients
+    await varDateTime.set_writable()
+    #await varOutput.set_writable()
+    await varBConsum.set_writable()
+    #await varMConsum.set_writable()
+    #await varPConsum.set_writable()
+    
+    
+    await server.nodes.objects.add_method(ua.NodeId('ServerMethod', 2), ua.QualifiedName('ServerMethod', 2), func, [ua.VariantType.Int64], [ua.VariantType.Int64])
+    _logger.info('Starting server!')
+    async with server:
+        while True:
+            for row in sensor_data.itertuples():
+                valDateTime = row[1]
+                #valOutput = row[2]
+                valBConsum = row[2]
+                #valMConsum = row[4]
+                #valPConsum = row[5]
+                
+                print(valDateTime)
+                
+                #_logger.info('Set value of %s to %.1f', myvar, new_val)
+                await varDateTime.write_value(valDateTime)
+                #await varOutput.write_value(valOutput)
+                await varBConsum.write_value(valBConsum)
+                #await varMConsum.write_value(valMConsum)
+                #await varPConsum.write_value(valPConsum)
+                
+                await asyncio.sleep(1)
+                
+if __name__ == '__main__':
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    asyncio.run(main(), debug=True)
